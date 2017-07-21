ad6caf30840a99498b6a74035451246b7de9df80
rstyczynski <rstyczynski@dhcp-ukc1-twvpn-1-vpnpool-10-175-166-223.vpn.oracle.com>
2017-07-22 00:07:26 +0200

    .

--- CacheWebClient/src/view/TrivialCacheDisplay.java
+++ CacheWebClient/src/view/TrivialCacheDisplay.java
@@ -1,9 +1,13 @@
 package view;
 
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.List;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -14,37 +18,34 @@ import javax.servlet.http.HttpServletResponse;
 import model.TrivialRecord;
 
 public class TrivialCacheDisplay extends HttpServlet {
+    @SuppressWarnings("compatibility:3309922168865781288")
+    private static final long serialVersionUID = 1L;
     private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
 
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
     }
 
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response) 
-      throws ServletException, IOException {
+
+    @SuppressWarnings("unchecked")
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType(CONTENT_TYPE);
         PrintWriter out = response.getWriter();
-        
-        
-        List <TrivialRecord> teams = (List<TrivialRecord>) new model.TrivialCacheDump().getDataFromCache();
-        
+
         out.println("<html>");
         out.println("<head><title>Cache reader</title></head>");
         out.println("<body>");
         out.println("<p/>");
-        out.println("<table>");
 
-        for(int i=0; i<teams.size();i++){
-            out.println("<tr><td>");
-            out.println(i+1);
-            out.println("</td>");
-            out.println("<td>");
-            out.println(teams.get(i));
-            out.println("</td>");
-            out.println("</tr>");
+        NamedCache cache = CacheFactory.getCache("trivialCache");
+
+        Iterator<Map.Entry<String, TrivialRecord>> it;
+        it = cache.entrySet().iterator();
+        while (it.hasNext()) {
+            out.println(it.next().getValue());
+            out.println("</br>");
         }
-        out.println("</table>");
+
         out.println("</body>");
         out.println("</html>");
         out.close();
--- CacheWebClient/src/view/TrivialCacheWarmer.java
+++ CacheWebClient/src/view/TrivialCacheWarmer.java
@@ -1,5 +1,8 @@
 package view;
 
+import com.tangosol.net.CacheFactory;
+import com.tangosol.net.NamedCache;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,6 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.TrivialRecord;
+
 public class TrivialCacheWarmer extends HttpServlet {
     private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
 
@@ -16,22 +21,30 @@ public class TrivialCacheWarmer extends HttpServlet {
         super.init(config);
     }
 
+    @SuppressWarnings("unchecked")
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            response.setContentType(CONTENT_TYPE);
-            PrintWriter out = response.getWriter();
-            out.println("<html>");
-            out.println("<head><title>Cache warmer</title></head>");
-            out.println("<body>");
-            out.println("<p>Filling the cache.</p>");
-            
-            new model.TrivialCacheWarmer().fillCacheWithSampleData();
-            
-            out.println("<p>Done.</p>");
-            
-            out.println("</body>");
-            out.println("</html>");
-            out.close();
-        }
-    
-    
+        response.setContentType(CONTENT_TYPE);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Cache warmer</title></head>");
+        out.println("<body>");
+        out.println("<p>Filling the cache.</p>");
+
+        NamedCache cache = CacheFactory.getCache("trivialCache");
+        cache.put("1", new TrivialRecord("Raz"));
+        cache.put("2", new TrivialRecord("Dwa"));
+        cache.put("3", new TrivialRecord("Trzy"));
+        cache.put("4", new TrivialRecord("Cztery"));
+        cache.put("5", new TrivialRecord("Pięć"));
+        cache.put("6", new TrivialRecord("Sześć"));
+        cache.put("7", new TrivialRecord("Siedem"));
+
+        out.println("<p>Done.</p>");
+
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+
+
 }
